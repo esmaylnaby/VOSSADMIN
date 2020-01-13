f374e6ab3c4205fdebe5770057a0cf979e61a45d
esmaylnaby <esmaylnaby@gmail.com>
2020-01-13 12:47:01 +0500

    Updates

---
+++ precache-manifest.04180613360a174506376dabe371da3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10daec6c5b41ff13b70119421c513f4",
+    "url": "/VOSSADMIN/index.html"
+  },
+  {
+    "revision": "cca40dde17621eb30343",
+    "url": "/VOSSADMIN/static/css/main.95b3e173.chunk.css"
+  },
+  {
+    "revision": "8ccc54c478006db0a3a4",
+    "url": "/VOSSADMIN/static/js/2.ca468b90.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/VOSSADMIN/static/js/2.ca468b90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cca40dde17621eb30343",
+    "url": "/VOSSADMIN/static/js/main.0ff60950.chunk.js"
+  },
+  {
+    "revision": "ab620f80bc9e6a8e30b8",
+    "url": "/VOSSADMIN/static/js/runtime-main.205b05ca.js"
+  },
+  {
+    "revision": "12f98d8bd3166de38cdab9163f3e4017",
+    "url": "/VOSSADMIN/static/media/logo.12f98d8b.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/VOSSADMIN/static/media/logo.ee7cd8ed.svg"
+  }
+]);
